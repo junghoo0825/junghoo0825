17002bbbd70483cd08181e5616625792b13ac843
junghoo0825 <81668830+junghoo0825@users.noreply.github.com>
2021-03-31 14:20:16 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,2 +1,18 @@
-let a = [10, 20, 30, 40, 50]
-basic.showNumber(a[0])
+input.onButtonPressed(Button.A, function () {
+    _1저정 = randint(0, 9)
+    basic.showString("" + (음식[_1저정]))
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (음식[_1저정]))
+    basic.showString("" + (음료수[_2저장]))
+})
+input.onButtonPressed(Button.B, function () {
+    _2저장 = randint(0, 2)
+    basic.showString("" + (음료수[_2저장]))
+})
+let _2저장 = 0
+let _1저정 = 0
+let 음료수: string[] = []
+let 음식: string[] = []
+음식 = ["chicken", "pizza", "spaghetti", "curry", "sweet potato", "ice cream", "houce rice", "pork cutlet", "tteokbokki", "ramen"]
+음료수 = ["coke", "cider", "water"]
